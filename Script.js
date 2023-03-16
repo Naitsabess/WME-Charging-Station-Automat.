baa45b10d4a4845afa0a3f717747196b748b2b3e
Naitsabess <118657630+Naitsabess@users.noreply.github.com>
2023-03-16 15:42:41 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,12 @@
 const SCRIPT_VERSION = 0.1;
 
+let openUpdateRequests = [];
+
+let style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = "#CSA-tab > h1 {font-size: 1.2em; margin-top: -20px;}";
+    document.getElementsByTagName("head")[0].appendChild(style);
+
 (function() {
     'use strict';
 
@@ -16,24 +23,37 @@ const SCRIPT_VERSION = 0.1;
         const {tabLabel, tabPane} = W.userscripts.registerSidebarTab("Charging-Station-Automat.");
         W.userscripts.waitForElementConnected(tabLabel).then(() => {
             tabLabel.innerText = "CSA Script";
+        });
+        
+        W.userscripts.waitForElementConnected(tabPane).then(() => {
             tabPane.id = "CSA-tab";
             tabPane.innerHTML = '<h1>CSA-Charging-Station-Automat.</h1>';
             tabPane.append(document.createElement("hr"));
-
-            console.log(getPlaceUpdateRequests());
-        });
+        }
     }
-
-    let style = document.createElement("style");
-    style.type = "text/css";
-    style.innerHTML = "#CSA-tab > h1 {font-size: 1.2em; margin-top: -20px;}";
-    document.getElementsByTagName("head")[0].appendChild(style);
-
-
+                                                            
     function getPlaceUpdateRequests() {
-        const updateRequests = W.model.venues.objects;
-        updateRequests.filter(obj => obj.isUpdateRequest);
-        return updateRequests;
-    }
+    const updateRequests = W.model.venues.objects;
+    const openRequests = updateRequests.filter(obj => obj.isUpdateRequest && obj.state === 1);
+    openUpdateRequests = openRequests.map(obj => {
+        return {
+            id: obj.attributes.id,
+            name: obj.attributes.name,
+            address: obj.attributes.street,
+            provider: obj.attributes.providerID
+        };
+    });
+    return openUpdateRequests;
+}
+    
+    function populateOpenUpdateRequestsList() {
+    const list = document.createElement("ul");
+    openUpdateRequests.forEach(request => {
+        const listItem = document.createElement("li");
+        listItem.innerText = `${request.name} (${request.address})`;
+        list.appendChild(listItem);
+    });
+    return list;
+}
 
 })();

fa9b3c0ce3047b98b253e5c8d3222d9c3a783753
Naitsabess <118657630+Naitsabess@users.noreply.github.com>
2023-03-19 00:08:41 +0100

    Added results sidepane tab state
    Added a new tab state that shows the scan results after clicking the scan button.
    Logic added for retrieving and filtering charging station venues.

--- Script.js
+++ Script.js
@@ -14,13 +14,19 @@
 // Version history
 // v 0.1 - Initial version of the script
 
-/* global W, $ */
+/* global W, $, WazeWrap*/
 
 
 (function() {
     'use strict';
 
+
+    // -------------------------- Basic settings -------------------------------------------
+    const SCRIPT_NAME = GM_info.script.name;
     const SCRIPT_VERSION = GM_info.script.version;
+    const SCRIPT_AUTHOR = GM_info.script.author;
+    const RELEASE_NOTES = "Initial version of the script";
+    const DEBUG = true;
     const SETTINGS = {
         default: {
             language: "en",
@@ -28,72 +34,129 @@
     }
     const STRINGS = {
         "en": {
-            scanArea: "Scan Area",
+            scan_Area: "Scan Area",
+            results_table_heading: "Charging stations:",
+            venue_no_name: "No name",
+            venue_no_street_name: "No street",
         },
         "de": {
             scanArea: "Suche im Gebiet",
+            results_table_heading: "Ladestationen:",
+            venue_no_name: "Ohne Name",
+            venue_no_street_name: "keine Straße",
         }
     }
 
-    let openUpdateRequests = [];
+    // -------------------------- Variables needed for functionality -------------------------
+    let chargingStationsInView = [];
+    let chargingStationsUpdateRequests = [];
     let language = SETTINGS.default.language;
-    let style = document.createElement("style");
-    style.type = "text/css";
-    //style.append("#CSA-tab {heigth: 100%}");
-    style.append("#CSA-tab > h1 {font-size: 1.2em; margin-top: -20px;}");
-    document.getElementsByTagName("head")[0].appendChild(style);
 
+    // -------------------------- Functions --------------------------------------------------
 
+    function showResultsInSidebar(tabPane) {
+        DEBUG && console.log(SCRIPT_NAME + ": Loading results sidebar...");
+        document.getElementById("scan-button").style = "display: none";
+        // check if it's already created by a previous search
+        let resultsSidebar = document.getElementById("results-sidebar-state");
+        if (resultsSidebar) {
+            tabPane.removeChild(resultsSidebar);
+        }
+        resultsSidebar = document.createElement("div");
+        resultsSidebar.id = "results-sidebar-state";
+        tabPane.appendChild(resultsSidebar);
 
-    function getOpenUpdateRequests() {
-    const updateRequests = W.model.mapUpdateRequests.getObjectArray();
-    const openRequests = updateRequests.filter(obj => obj.isUpdateRequest && obj.state === 1);
-    openUpdateRequests = openRequests.map(obj => {
-        return {
-            id: obj.attributes.id,
-            name: obj.attributes.name,
-            address: obj.attributes.street,
-            provider: obj.attributes.providerID
-        };
-    });
+        const selectProvider = document.createElement("select");
+        selectProvider.id = "select-provider";
+        selectProvider.name = "provider";
+        resultsSidebar
 
-    return openUpdateRequests;
+        // ------------------------------- Beginn drawing the result table ---------------------------------------------
+
+        console.log(chargingStationsInView[0].getAddress());
+        let searchResultsListHTML =
+            `<h2>${STRINGS[language].results_table_heading}</h2>
+            <table style="background-color: #eeeee4; font-size: 0.9em;">
+                <tbody>`;
+
+        // one row for each charging station entry
+        for (let i = 0; i < chargingStationsInView.length; i++) {
+            const venueName = (chargingStationsInView[i].getName() === null) ? STRINGS[language].venue_no_name : chargingStationsInView[i].getName();
+            const venueStreetName = (chargingStationsInView[i].getAddress().isEmptyStreet()) ? STRINGS[language].venue_no_street_name : chargingStationsInView[i].getAddress().getStreetName();
+            const venueHouseNumber = (chargingStationsInView[i].getAddress().getHouseNumber() === null) ? "" : " " + chargingStationsInView[i].getAddress().getHouseNumber();
+
+            // adds number at the beginn of the row with some styling
+            searchResultsListHTML +=
+                `<tr style="border: 1px solid black;">
+                     <td style="min-width: 25px; text-align: center; border-right: 1px solid black;">${i + 1}</td>`;
+
+            // adds the search results with name and street name / house number (if available)
+            searchResultsListHTML += `<td style="padding: 0 10px;">${venueName}<br><i>(${venueStreetName}${venueHouseNumber})</i></td>`;
+                 `</tr>`;
+        }// end for
+        searchResultsListHTML +=
+                `</tbody>
+            </table>`;
+        resultsSidebar.innerHTML += searchResultsListHTML;
+
+        // ------------------------------- End drawing the result table ---------------------------------------------------
     }
 
-    function populateOpenUpdateRequestsList() {
-
-        if (openUpdateRequests.length > 0) {
-            document.getElementById("scan-button").style = "display: none";
+    function mapScan() {
+        // search should produce a unique search result
+        if (chargingStationsInView.length !== 0) {
+            chargingStationsInView.length = 0;
         }
 
-        const list = document.createElement("ul");
-        openUpdateRequests.forEach(request => {
-            const listItem = document.createElement("li");
-            listItem.innerText = `${request.name} (${request.address})`;
-            list.appendChild(listItem);
+        chargingStationsInView = (W.model.venues.getObjectArray().filter(obj => obj.isChargingStation() && obj.outOfScope === false));
+        chargingStationsUpdateRequests = chargingStationsInView.filter(obj => !obj.isApproved());
+        DEBUG && console.log(SCRIPT_NAME + ": Map scan completed. Results found: ");
+        DEBUG && console.dir(chargingStationsInView);
+        }
+
+    function loadDefaultSidebar(tabPane) {
+        const defaultSidebar = document.createElement("div");
+        defaultSidebar.id = "default-sidebar-state";
+        tabPane.appendChild(defaultSidebar);
+
+        const scanButton = document.createElement("button");
+        scanButton.id = "scan-button";
+        scanButton.innerText = STRINGS[language].scan_Area;
+        scanButton.style = "display: block; margin: 40% auto 85% auto; background-color: limegreen; color: white; border-radius: 4px; border: none";
+        scanButton.addEventListener("click", () => {
+            mapScan();
+            showResultsInSidebar(tabPane);
         });
-        return list;
+        defaultSidebar.appendChild(scanButton);
     }
 
     function initialize() {
+
+        WazeWrap.Interface.ShowScriptUpdate("WME Charging.Station-Automat.", SCRIPT_VERSION, RELEASE_NOTES, "Greasyfork Link", "Forum Link");
+
+        let style = document.createElement("style");
+        style.type = "text/css";
+        style.append("#CSA-tab > * {margin-bottom: 0;}");
+        style.append(".spacer {padding-bottom: 10px;}");
+        style.append("#CSA-tab > h1 {font-size: 1.3rem; margin-top: -20px;}");
+        style.append("#CSA-tab > div > h2 {font-size: 1.1rem;}");
+        style.append("#CSA-tab > div > h2 {font-size: 1.1rem;}");
+        document.getElementsByTagName("head")[0].appendChild(style);
+
         const {tabLabel, tabPane} = W.userscripts.registerSidebarTab("Charging-Station-Automat.");
         W.userscripts.waitForElementConnected(tabLabel).then(() => {
             tabLabel.innerText = "CSA Script";
         });
 
         W.userscripts.waitForElementConnected(tabPane).then(() => {
+            // create header that stays on top
             tabPane.id = "CSA-tab";
             tabPane.innerHTML = '<h1>CSA-Charging-Station-Automat.</h1>';
-            tabPane.innerHTML += `version: ${SCRIPT_VERSION}`;
-            tabPane.append(document.createElement("hr"));
-            tabPane.appendChild(document.createElement("div"));
-
-            let scanButton = document.createElement("button");
-            scanButton.id = "scan-button";
-            scanButton.innerText = STRINGS[language].scanArea;
-            scanButton.style = "display: block; margin: 40% auto 50% auto; background-color: limegreen; color: white; border-radius: 4px; border: none";
-            scanButton.addEventListener("click", populateOpenUpdateRequestsList);
-            tabPane.appendChild(scanButton);
+            tabPane.innerHTML += `<p>version: ${SCRIPT_VERSION}<br>by ${SCRIPT_AUTHOR}</p>`;
+            tabPane.innerHTML += `<hr class="spacer">`;
+            loadDefaultSidebar(tabPane);
+            DEBUG && console.log(SCRIPT_NAME + ": Initialized");
+            DEBUG && console.dir(W.model.venues);
         })
     }
 
